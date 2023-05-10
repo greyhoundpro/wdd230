997e11c4ba824b4dd2cccbb1b3ef7f01b26380c3
greyhoundpro <jayveegalosmo28@gmail.com>
2023-05-10 23:31:01 +0800

    edited css of chamber

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -2,11 +2,17 @@ const d = new Date();
 const year = d.getFullYear();
 document.querySelector("#year").innerHTML = year;
 
-const options = {month: 'numeric', day: 'numeric', year: 'numeric'};
+const options = {month: 'long', day: 'numeric', year: 'numeric'};
+const options1 = {weekday: 'long', day: 'numeric', month: 'long',  year: 'numeric'};
 const date = new Date(document.lastModified);
+const currentDate = new Date();
+document.querySelector('#date1').textContent = currentDate.toLocaleDateString('en-UK', options1);
 document.querySelector('#modified-date').textContent = date.toLocaleDateString('en-US', options);
 document.querySelector('#modified-time').textContent = new Date(document.lastModified).toLocaleTimeString('en-US');
 
+
+
+// Mobile menu toggle action
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar-menu');
 const navLogo = document.querySelector('.logo');
