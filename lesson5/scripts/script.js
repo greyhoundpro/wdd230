00f3a5444cc83768b3473d4ffb4f16693180967e
greyhoundpro <jayveegalosmo28@gmail.com>
2023-05-18 11:40:59 +0800

    edited css for bom html

--- lesson5/scripts/script.js
+++ lesson5/scripts/script.js
@@ -1,3 +1,13 @@
+const d = new Date();
+const year = d.getFullYear();
+document.querySelector("#year").innerHTML = year;
+
+const options = {month: 'numeric', day: 'numeric', year: 'numeric'};
+const date = new Date(document.lastModified);
+document.querySelector('#modified-date').textContent = date.toLocaleDateString('en-US', options);
+document.querySelector('#modified-time').textContent = new Date(document.lastModified).toLocaleTimeString('en-US');
+
+
 const list = document.querySelector('ul');
 const input = document.querySelector('input');
 const button = document.querySelector('.inputBtn');
