a9d6ec7175f31875df414c69cf876864f5205e52
greyhoundpro <jayveegalosmo28@gmail.com>
2023-05-29 13:40:46 +0800

    completed lazyload

--- lesson7/app.js
+++ lesson7/app.js
@@ -9,14 +9,38 @@ document.querySelector('#modified-date').textContent = date.toLocaleDateString('
 document.querySelector('#modified-time').textContent = new Date(document.lastModified).toLocaleTimeString('en-US');
 
 
-let imagesToLoad = document.querySelectorAll('img[data-src]');
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const imgOptions = {
+    threshold: 0, 
+    rootMargin: "0px 0px -250px 0px"
+}
+
 const loadImages = (image) => {
-    image.setAttribute("src", image.getAttribute("data-src"));
+    image.setAttribute('src', image.getAttribute('data-src'));
     image.onload = () => {
-        image.removeAttribute("data-src");
+        image.removeAttribute('data-src');
     };
 };
 
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-});
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+        if (item.isIntersecting) {
+            loadImages(item.target);
+            observer.unobserve(item.target);
+        }
+        });
+    }, imgOptions);
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+} 
+else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
+
+
