a56fa851d0b82c74df4b4978c983cd5018b08179
greyhoundpro <jayveegalosmo28@gmail.com>
2023-05-31 13:46:11 +0800

    edited days since last visit in js

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -48,18 +48,29 @@ if (d.getDay() == 1 || d.getDay() == 2) {
 // Number of Site Visits
 
 let numberOfVisits = localStorage.getItem("numberOfVisits");
+let previousVisit = localStorage.getItem("lastVisit");
 
 if (!numberOfVisits) {
     numberOfVisits = 0;
 }
 numberOfVisits++;
 
-localStorage.setItem("numberOfVisits", numberOfVisits);
-document.querySelector('.visit').innerHTML = "This is your visit #" + numberOfVisits + " on this webpage";
+const minute = 1000 * 60;
+const hour = minute * 60;
+const day = hour * 24;
+const Year = day * 365;
 
+const lastVisit = Date.now();
+localStorage.setItem("numberOfVisits", numberOfVisits);
+localStorage.setItem("lastVisit", lastVisit);
+document.querySelector('.visit').innerHTML = "This is your visit #" + numberOfVisits + ".";
+
+let daysSinceLast = Math.round((lastVisit - previousVisit) / day);
+
+if (numberOfVisits >= 2) {
+    document.querySelector('#daysSinceLast').innerHTML = "Days since last visit: " + daysSinceLast + ".";
+}
 if (numberOfVisits < 2) {
     document.querySelector('.visit').innerHTML = "Thanks for visiting!"
 }
-
-
 
