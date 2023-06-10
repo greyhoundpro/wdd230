d722253a8000df17d1a138b29562c5152e4308c0
greyhoundpro <jayveegalosmo28@gmail.com>
2023-06-10 08:52:26 +0800

    edited join page

--- chamber/scripts/formload.js
+++ chamber/scripts/formload.js
@@ -1,5 +1,5 @@
 const options2 = {month: 'long', day: 'numeric', year: 'numeric'};
 const dateToday = new Date();
 const hiddenp = document.querySelector('#hidDateAndTime');
-hiddenp.innerHTML = dateToday.toLocaleDateString('en-US', options2) + " " + dateToday.toLocaleTimeString('en-US');
+hiddenp.value = dateToday.toLocaleDateString('en-US', options2) + " " + dateToday.toLocaleTimeString('en-US');
 
