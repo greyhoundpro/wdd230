267c945ba7665fd7bdbc752bff320f0998accf98
greyhoundpro <jayveegalosmo28@gmail.com>
2023-06-20 11:40:40 +0800

    finished weather API assignment

---
+++ lesson10/scripts/weatherapi.js
@@ -0,0 +1,36 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&appid=e375ffcf162eb1292ef6ec40581db610&units=imperial';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data  = await response.json();
+            console.log(data); //this is for testing the call
+            displayResults(data);
+        }
+        else {
+            throw Error(await response.text());
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`;
+const desc = weatherData.weather[0].description;
+
+weatherIcon.setAttribute('src', iconsrc);
+weatherIcon.setAttribute('alt', desc);
+captionDesc.textContent = desc;
+
+}
