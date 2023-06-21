3417d953d9e6542bec7df5f919331bc95e28ee24
greyhoundpro <jayveegalosmo28@gmail.com>
2023-06-21 14:49:43 +0800

    almost finished chamber home

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,19 +1,31 @@
+// ------------ WEATHER API ------------- 
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?id=1731685&appid=e375ffcf162eb1292ef6ec40581db610&units=imperial';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data  = await response.json();
+            console.log(data); //this is for testing the call
+            displayResults(data);
+        }
+        else {
+            throw Error(await response.text());
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+
 const temperature = document.querySelector('#temperature');
 const windSpeed = document.querySelector('#windSpeed');
-
-const tempC = parseFloat(temperature.innerText);
-const speed = parseFloat(windSpeed.innerText);
-const tempF = convertCelsiusToF(tempC);
-
-const windChill = getwindChill(tempF, speed);
-const windCh = Math.round(windChill * 100)/ 100;
-
-if (tempF <= 50 & speed > 3) {
-    showWindChill(windCh);
-} 
-else {
-    NA();
-}
+const weatherIcon = document.querySelector('#weather-icon');
+const wDescription = document.querySelector('#wDescription')
 
 // Functions
 
@@ -28,6 +40,7 @@ function convertCelsiusToF(celsius) {
     return Math.floor(celsius * (9/5) + 32);
 }
 
+
 function NA() {
     const result = document.querySelector('#windChill');
     result.textContent = "N/A";
@@ -37,3 +50,34 @@ function showWindChill(windCh) {
     const result = document.querySelector('#windChill');
     result.textContent = windCh + " °F";
 }
+
+// Function for the API
+
+function displayResults(weatherData) {
+    const speed = weatherData.wind.speed;
+    const tempF = weatherData.main.temp;
+    const tempC = (tempF - 32) * 5/9;
+
+    temperature.textContent = `${Math.floor(tempC)} \u00B0C`;
+    windSpeed.textContent = `${speed} mph`;
+
+    const windChill = getwindChill(tempF, speed);
+    const windCh = Math.round(windChill * 100)/ 100;
+
+    if (tempF <= 50 & speed > 3) {
+        showWindChill(windCh);
+    } 
+    else {
+        NA();
+        }
+    
+
+    const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
+    const desc = weatherData.weather[0].description;
+    wDescription.textContent = desc;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+
+}
+
